4159f547270e27cdd36f08823cff63f3f63fface
Damian Gurdak <damiangurdakk@gmail.com>
2023-03-10 04:07:47 +0100

    add structure of the application - logic

---
+++ src/components/pages/About/About.js
@@ -0,0 +1,10 @@
+// import styles from './About.module.scss'
+
+const About = () => {
+  return (
+    <div>
+      <p>About</p>
+    </div>
+  );
+};
+export default About;
---
+++ src/components/pages/PostAdd/PostAdd.js
@@ -0,0 +1,11 @@
+// import styles from './PostAdd.module.scss';
+
+const PostAdd = () => {
+  return (
+    <div>
+      <h1>PostAdd</h1>
+    </div>
+  );
+};
+
+export default PostAdd;
